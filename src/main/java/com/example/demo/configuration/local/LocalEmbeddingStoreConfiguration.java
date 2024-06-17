15678454753eef30f7345d3e9950db29c17d59cd
Julien Dubois <julien.dubois@gmail.com>
2024-06-17 22:09:27 +0200

    Fix using QDrant locally

--- src/main/java/com/example/demo/configuration/local/LocalEmbeddingStoreConfiguration.java
+++ src/main/java/com/example/demo/configuration/local/LocalEmbeddingStoreConfiguration.java
@@ -3,18 +3,37 @@ package com.example.demo.configuration.local;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.qdrant.QdrantEmbeddingStore;
+import io.qdrant.client.QdrantClient;
+import io.qdrant.client.QdrantGrpcClient;
+import io.qdrant.client.grpc.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import java.util.concurrent.ExecutionException;
 
 @Configuration
 @Profile("local")
 public class LocalEmbeddingStoreConfiguration {
 
+    private static final String COLLECTION_NAME = "kbindex";
+
     @Bean
-    EmbeddingStore<TextSegment> embeddingStore() {
+    EmbeddingStore<TextSegment> embeddingStore() throws ExecutionException, InterruptedException {
+        QdrantClient client =
+                new QdrantClient(QdrantGrpcClient.newBuilder("localhost", 6334, false).build());
+
+        if (!client.listCollectionsAsync().get().contains(COLLECTION_NAME)) {
+            client.createCollectionAsync(COLLECTION_NAME,
+                            Collections.VectorParams.newBuilder()
+                                    .setDistance(Collections.Distance.Cosine)
+                                    .setSize(384)
+                                    .build())
+                    .get();
+        }
+
         return QdrantEmbeddingStore.builder()
-                .collectionName("kbindex")
+                .collectionName(COLLECTION_NAME)
                 .host("localhost")
                 .port(6334)
                 .build();
