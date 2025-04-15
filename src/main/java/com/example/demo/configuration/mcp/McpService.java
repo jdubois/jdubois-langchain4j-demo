c8d3ec83448ae3b2b3f90d435843222166cc41fe
Julien Dubois <julien.dubois@gmail.com>
2025-04-15 11:11:43 +0200

    Simplify demo

--- src/main/java/com/example/demo/configuration/mcp/McpService.java
+++ src/main/java/com/example/demo/configuration/mcp/McpService.java
@@ -6,7 +6,6 @@ import dev.langchain4j.mcp.client.McpClient;
 import dev.langchain4j.mcp.client.transport.McpTransport;
 import dev.langchain4j.mcp.client.transport.stdio.StdioMcpTransport;
 import dev.langchain4j.service.tool.ToolProvider;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -17,13 +16,10 @@ import java.util.List;
 @Lazy
 public class McpService {
 
-    @Value("${AZURE_CREDENTIALS:}")
-    private String azureCredentials;
-
     @Bean
     public ToolProvider mcpToolProvider() {
         McpTransport transport = new StdioMcpTransport.Builder()
-                .command(List.of("docker", "run", "-e", "AZURE_CREDENTIALS=" + azureCredentials, "-i", "ghcr.io/jdubois/azure-cli-mcp:latest"))
+                .command(List.of("java", "-jar", "/Users/julien/workspace/azure-cli-mcp/target/azure-cli-mcp.jar"))
                 .logEvents(true)
                 .build();
 
