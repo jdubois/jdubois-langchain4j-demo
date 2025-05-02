c95c9d8cee3151ae23a4aba3977b9a16eaa0cc6e
Julien Dubois <julien.dubois@gmail.com>
2025-05-02 13:10:51 +0200

    Special modifications for Build 2025

--- docker_build_and_push.sh
+++ docker_build_and_push.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker buildx build --platform linux/amd64 -t jdubois/jdubois-langchain4j-demo:8 --push .
+docker buildx build --platform linux/amd64 -t jdubois/jdubois-langchain4j-demo:9 --push .
